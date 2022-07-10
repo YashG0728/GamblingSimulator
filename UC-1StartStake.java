3a2547fe40ff49d39912b997fffd8423d132e0f4
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-10 14:54:38 +0530

    Update and rename StartStake.java to UC-1StartStake.java

---
+++ UC-1StartStake.java
@@ -0,0 +1,16 @@
+
+public class StartStake {
+
+	public static final int EveryDayStake = 100;
+ 	public static final int bet = 1;
+
+   	public static void main(String[] args) {
+
+        System.out.println("---Welcome To Gambling Simulator---");
+
+        }
+    }
+
+	
+
+
